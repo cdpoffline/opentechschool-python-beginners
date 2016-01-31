a2a3ac490dcf2d30e2825206333013c8a39f50bc
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-01-31 14:17:28 +0000

    downloading when newer

--- bin/download_installers.sh
+++ bin/download_installers.sh
@@ -4,14 +4,13 @@ cd "`dirname \"$0\"`"
 
 cd ../web
 
-rm -r downloads
-mkdir downloads
+mkdir -p downloads
 cd downloads
 
 version=3.5.1
 
-wget https://www.python.org/ftp/python/${version}/python-${version}.exe
-wget https://www.python.org/ftp/python/${version}/python-${version}-amd64.exe
-wget https://www.python.org/ftp/python/${version}/python-${version}-macosx10.6.pkg
-wget https://www.python.org/ftp/python/${version}/python-${version}-macosx10.5.pkg
+wget -c -N https://www.python.org/ftp/python/${version}/python-${version}.exe
+wget -c -N https://www.python.org/ftp/python/${version}/python-${version}-amd64.exe
+wget -c -N https://www.python.org/ftp/python/${version}/python-${version}-macosx10.6.pkg
+wget -c -N https://www.python.org/ftp/python/${version}/python-${version}-macosx10.5.pkg
 
